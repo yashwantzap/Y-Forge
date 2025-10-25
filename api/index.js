98d7f592e3c82f689fbe67f0a20b05fc2118fefa
Yash <yashwantnutulapati@gmail.com>
2025-10-25 09:55:21 +0530

    Commit

--- api/index.js
+++ api/index.js
@@ -37,7 +37,7 @@ try {
   transporter = nodemailer.createTransport({
     host: process.env.SMTP_HOST,
     port: parseInt(process.env.SMTP_PORT, 10),
-    secure: false,
+    secure: true,
     auth: {
       user: process.env.SMTP_USER,
       pass: process.env.SMTP_PASS,
